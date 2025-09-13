080f952e0a81ab21c5e02c0306ace921605ccff9
stonebird <1245863260@qq.com>
2025-09-13 23:21:25 +0800

    feat:request

--- src/main/java/com/example/spring_boot_demo/service/InstanceService.java
+++ src/main/java/com/example/spring_boot_demo/service/InstanceService.java
@@ -1,9 +1,6 @@
 package com.example.spring_boot_demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.example.spring_boot_demo.dao.InstanceDao;
 
 @Service
 public interface InstanceService {
